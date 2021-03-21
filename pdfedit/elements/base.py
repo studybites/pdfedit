f534f9f7f782008a5e8f459e152f23cee9ecb714
Alon (PC) <downtown2u@gmail.com>
2021-03-22 01:29:56 +0200

    ✨ Added basic `Element` and `Section`

---
+++ pdfedit/elements/base.py
@@ -0,0 +1,27 @@
+from pdfedit.utils import types
+
+
+class Element:
+    """ A base-class for elements that can be included inside a section. """
+
+
+class Section:
+    """ The canvas can contain multiple sections. Each section can contain
+    multiple elements, such as images, text, hyperlinks, etc. """
+
+    def __init__(self,
+                 *elements: Element,
+                 anchor,
+                 position,
+                 ):
+        self.elements = elements
+        self.anchor = anchor
+        self.position = position
+
+    @property
+    def width(self,) -> types.PositiveNumber:
+        pass
+
+    @property
+    def height(self,) -> types.PositiveNumber:
+        pass
