288464a0a5d6b62eed743ff56b39864ed2df7a36
Alon (PC) <downtown2u@gmail.com>
2021-03-23 10:48:38 +0200

    ✨ Added alignment implementation

--- pdfedit/utils/align.py
+++ pdfedit/utils/align.py
@@ -1,18 +1,28 @@
 """ Different align directions. Used by different elements and the interpretor
 to align the element! """
 
+from pdfedit.utils.types import Percentage
+
 
 class Alignment:
-    pass
+
+    def __init__(self, relative_x: Percentage):
+        self.relative_x = relative_x
 
 
 class AlignToCenter(Alignment):
-    pass
+
+    def __init__(self,):
+        super().__init__(0.5)
 
 
 class AlignToRight(Alignment):
-    pass
+
+    def __init__(self,):
+        super().__init__(1)
 
 
 class AlignToLeft(Alignment):
-    pass
+
+    def __init__(self,):
+        super().__init__(0)
